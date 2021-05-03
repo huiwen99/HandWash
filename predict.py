361cb37f75aa07161703e5c6dff1821648732911
joyobo <joeyyeokl@gmail.com>
2021-05-03 07:26:02 +0000

    modified predict.py to include device param

--- predict.py
+++ predict.py
@@ -7,15 +7,18 @@ parser = argparse.ArgumentParser(description="Predict action")
 parser.add_argument("--arch", type=str, default="alexnet", help="set architecture")
 parser.add_argument("--checkpoint", type=str, default=None, help="checkpoint to load model")
 parser.add_argument("--video_path", type=str, default = None, help="Video path")
+parser.add_argument("--cuda", type=bool, default=False, help="enable cuda training")
 
 # get arguments
 args = parser.parse_args()
 arch = args.arch
 checkpoint = args.checkpoint
 video_path = args.video_path
+cuda=args.cuda
 
-# set cpu
-device = torch.device("cpu")
+# set cpu / gpu
+use_cuda = cuda and torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
 
 model = build_model(arch)
 model.to(device)
